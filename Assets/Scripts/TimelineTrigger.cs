eed31bf745cd350ca9d9ab48347a28398609ab1c
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-11-23 11:22:36 -0400

    Started Trigger scripts

---
+++ Assets/Scripts/TimelineTrigger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
